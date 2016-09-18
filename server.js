d304774b7381061fde229a40c3059984de1b929c
ark2290 <ark2290@gmail.com>
2016-09-18 14:55:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+ res.send("This is my first route");
+});
+
+app.get('/article2', function (req, res) {
+ res.send("This is my second route");
+});
+
+app.get('/article3', function (req, res) {
+res.send("This is my third route");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
