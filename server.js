34f1ee08ec12680f7eeb73e36dbfc38a978534a9
ark2290 <ark2290@gmail.com>
2016-09-24 16:15:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,13 +10,12 @@ app.get('/', function (req, res) {
 });
 
 var counter =0;
-app.get('/counter',function (req,res){
-   counter=counter+1;
-   res.send(counter.toString());
-});
+
 
 app.get('/profile', function (req, res) {
  res.sendFile(path.join(__dirname, 'ui', 'profile.html'));
+  counter=counter+1;
+   res.send(counter.toString());
 });
 
 app.get('/article2', function (req, res) {
