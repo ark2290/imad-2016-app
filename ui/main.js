5e455bc9468dec18fe7bbbd3e7aa09f457ec3ee8
ark2290 <ark2290@gmail.com>
2016-09-24 14:56:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var input=prompt("Enter your name");
+alert("Hiii"+input+"");
+
+var image=document.getElementById("madi");
+image.onclick=function(){
+    image.style.marginRight="300px"
+    
+    
+}
